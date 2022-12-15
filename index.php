04d331facff2f4700bdea3d457b21da46d2093a4
sanjana <sanjana@sanjanagoldar11.com>
2022-12-15 22:00:39 +0530

    operations

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
 <?php
+// this is string
+echo "this is my first php line <br>";
 
-echo "this is my first php line";
+//this is veriable
+$var = "this is some text<br>";
+echo "$var";
+
+//this concatination & normal string
+$fname = "sanjana";
+$sname = " goldar<br>";
+
+echo $fname.$sname."this is me<br>";
+
+//operators
+
+//arithmetic opretion
+
+$f=15;
+$s=5;
+//addition
+$value= $f+$s;
+echo "addition:".$value;
+
+//subtraction
+$value= $f-$s;
+echo "\nsubtraction:".$value;
+
+//multification
+$value= $f*$s;
+echo "\nmulti:".$value;
+
+//division
+$value= $f/$s;
+echo "\ndivision:".$value;
+
+//modulation [reminder]
+$value= $f%$s;
+echo "\nmodular:".$value;
 
 ?>
